d75b270010f021c31311de94badc9a258f7572e4
Jack Sparrow <jack@pirate.com>
2019-02-02 09:58:08 +0800

    Delete files

--- src/main/java/com/microcore/center/SlsCenterApplication.java
+++ src/main/java/com/microcore/center/SlsCenterApplication.java
@@ -4,7 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 
