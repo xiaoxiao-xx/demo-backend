a55210210b82e1ee7041db7d5d821021c0b32cea
Jack Sparrow <jack@pirate.com>
2019-02-19 14:01:10 +0800

    Add more basic source code for the project

---
+++ src/main/java/com/microcore/center/controller/DeviceController.java
@@ -0,0 +1,54 @@
+package com.microcore.center.controller;
+
+import com.microcore.center.service.PsmDeviceService;
+import com.microcore.center.vo.PsmDeviceVo;
+import com.microcore.center.vo.ResultVo;
+import com.microcore.common.constant.Constants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("deviceController")
+public class DeviceController {
+
+    @Autowired
+    private PsmDeviceService psmDeviceService;
+
+    @GetMapping("getDeviceList")
+    public ResultVo getDeviceList(@RequestParam String deviceId, @RequestParam String devtypeVal,
+                                  @RequestParam String state, @RequestParam String pageIndex,
+                                  @RequestParam String pageSize) {
+        return ResultVo.ok(psmDeviceService.getDeviceList(deviceId, devtypeVal, state));
+    }
+
+    @PostMapping("add")
+    public ResultVo addDevice(@RequestBody PsmDeviceVo vo) {
+        psmDeviceService.add(vo);
+        return ResultVo.ok();
+    }
+
+    @PostMapping("delete")
+    public ResultVo deleteDevice(@RequestParam String id) {
+        psmDeviceService.delete(id);
+        return ResultVo.ok();
+    }
+
+    @PostMapping("update")
+    public ResultVo updateDevice(@RequestBody PsmDeviceVo vo) {
+        psmDeviceService.update(vo);
+        return ResultVo.ok();
+    }
+
+    @PostMapping("enableDevice")
+    public ResultVo enableDevice(@RequestParam String id) {
+        psmDeviceService.setDeviceState(id, Constants.DEVICE_STATE_ENABLE);
+        return ResultVo.ok();
+    }
+
+    @PostMapping("disableDevice")
+    public ResultVo disableDevice(@RequestParam String id) {
+        psmDeviceService.setDeviceState(id, Constants.DEVICE_STATE_DISABLE);
+        return ResultVo.ok();
+    }
+
+}
---
+++ src/main/java/com/microcore/center/service/PsmDeviceService.java
@@ -0,0 +1,20 @@
+package com.microcore.center.service;
+
+import com.microcore.center.dto.PsmDeviceDto;
+import com.microcore.center.vo.PsmDeviceVo;
+
+import java.util.List;
+
+public interface PsmDeviceService {
+
+    List<PsmDeviceDto> getDeviceList(String deviceId, String devtypeVal, String state);
+
+    void add(PsmDeviceVo vo);
+
+    void delete(String deviceId);
+
+    void update(PsmDeviceVo vo);
+
+    void setDeviceState(String deviceId, String state);
+
+}
