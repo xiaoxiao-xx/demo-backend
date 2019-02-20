3cd82b741f28ce667e2a26885a218b5aa8f7710d
Administrator <Administrator@PKNMTWFFD9COKIL>
2019-02-20 16:29:53 +0800

    文件存储

--- src/main/java/com/microcore/center/controller/FileController.java
+++ src/main/java/com/microcore/center/controller/FileController.java
@@ -3,6 +3,7 @@ package com.microcore.center.controller;
 import java.io.File;
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.microcore.center.model.PsmFile;
+import com.microcore.center.service.FileService;
 import com.microcore.center.util.CommonUtil;
 import com.microcore.center.vo.ResultVo;
 
@@ -21,6 +24,9 @@ public class FileController {
 	@Value("${sls.FILE_PATH}")
 	private String filePath;
 
+	@Autowired
+	private FileService fileService ;
+	
 	@PostMapping("fileUpload")
 	@ResponseBody
 	public ResultVo fileUpload(@RequestParam("fileName") MultipartFile file) {
@@ -37,6 +43,13 @@ public class FileController {
 		}
 		try {
 			file.transferTo(dest); // 保存文件
+			PsmFile psmFile = new PsmFile();
+			psmFile.setId(fileId);
+			psmFile.setOldFileName(fileName);
+			psmFile.setSuffix(getSuffix(fileName));
+			psmFile.setStoreFileName(newFileName);
+			psmFile.setCrtDate(CommonUtil.getCurrentTime());
+			fileService.add(psmFile);
 			return ResultVo.ok(newFileName);
 		} catch (IllegalStateException e) {
 			e.printStackTrace();
---
+++ src/main/java/com/microcore/center/service/FileService.java
@@ -0,0 +1,7 @@
+package com.microcore.center.service;
+
+import com.microcore.center.model.PsmFile;
+
+public interface FileService {
+	public void add(PsmFile psmFile);
+}
