c620c0a8c6c44fad59bf259fe79f93b835e290e4
Jack Sparrow <jack@pirate.com>
2019-02-21 13:45:05 +0800

    Change the schedule config interfaces

--- src/main/java/com/microcore/center/service/impl/ScheduleConfigServiceImpl.java
+++ src/main/java/com/microcore/center/service/impl/ScheduleConfigServiceImpl.java
@@ -1,11 +1,15 @@
 package com.microcore.center.service.impl;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.microcore.center.mapper.PsmScheduleConfigMapper;
 import com.microcore.center.model.PsmScheduleConfig;
 import com.microcore.center.model.PsmScheduleConfigExample;
 import com.microcore.center.service.ScheduleConfigService;
+import com.microcore.center.util.CommonUtil;
 import com.microcore.center.vo.PsmScheduleConfigVo;
 import com.microcore.center.vo.ResultVo;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -41,11 +45,33 @@ public class ScheduleConfigServiceImpl implements ScheduleConfigService {
     }
 
     @Override
-    public ResultVo getScheduleConfigList() {
+    public ResultVo getScheduleConfigList(Integer pageIndex, Integer pageSize) {
         PsmScheduleConfigExample example = new PsmScheduleConfigExample();
         PsmScheduleConfigExample.Criteria criteria = example.createCriteria();
-        List<PsmScheduleConfig> psmScheduleConfigs = psmScheduleConfigMapper.selectByExample(example);
-        return ResultVo.ok(psmScheduleConfigs);
+
+        PageInfo<PsmScheduleConfig> scheduleConfigPageInfo = PageHelper.startPage(pageIndex, pageSize)
+                .doSelectPageInfo(() -> psmScheduleConfigMapper.selectByExample(example));
+
+        List<PsmScheduleConfig> configList = scheduleConfigPageInfo.getList();
+        List<PsmScheduleConfigVo> configVos = CommonUtil.listPo2VO(configList, PsmScheduleConfigVo.class);
+
+        PageInfo<PsmScheduleConfigVo> scheduleConfigVoPageInfo = CommonUtil.po2VO(scheduleConfigPageInfo, PageInfo.class);
+        scheduleConfigVoPageInfo.setList(configVos);
+
+        return ResultVo.ok(scheduleConfigVoPageInfo);
+    }
+
+    @Override
+    public void batchDelete(String idList) {
+        idList = idList.trim();
+        if (StringUtils.isEmpty(idList)) {
+            return;
+        }
+
+        String[] ids = idList.split(",");
+        for (String i : ids) {
+            delete(i);
+        }
     }
 
 }
