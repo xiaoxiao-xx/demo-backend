6ba1cfde21c03b309b45aee7d1441cd2bd048084
lmh <maxwell.lee@outlook.com>
2019-04-15 09:41:04 +0800

    Add a test case

--- task-system/src/test/java/com/microcore/center/cllient/HttpTemplateTest.java
+++ task-system/src/test/java/com/microcore/center/cllient/HttpTemplateTest.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class HttpTemplateTest extends TestCase {
@@ -24,6 +27,18 @@ public class HttpTemplateTest extends TestCase {
     public void testPost() {
         String s = httpTemplate.post("163.com", "80", "/", null, String.class);
         System.out.println(s);
+    }
+
+
+    @Test
+    public void testDateFormat() throws InterruptedException {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss SSS");
+        for (int i = 0; i < 10; i++) {
+            Date now = new Date();
+            String s = format.format(now);
+            System.out.println(s);
+            Thread.sleep(500L);
+        }
     }
 
 }
