4de439edf461b83416db9520fb0a4ba7e754727d
lmh <maxwell.lee@outlook.com>
2019-04-29 17:42:48 +0800

    New mapper files

---
+++ common/src/main/java/com/rainyhon/common/model/AlarmPolicyObjectExample.java
@@ -0,0 +1,402 @@
+package com.rainyhon.common.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlarmPolicyObjectExample {
+    /**
+     * alarm_policy_object
+     */
+    protected String orderByClause;
+
+    /**
+     * alarm_policy_object
+     */
+    protected boolean distinct;
+
+    /**
+     * alarm_policy_object
+     */
+    protected List<Criteria> oredCriteria;
+
+    /**
+     *
+     * @mbggenerated
+     */
+    public AlarmPolicyObjectExample() {
+        oredCriteria = new ArrayList<Criteria>();
+    }
+
+    /**
+     *
+     * @mbggenerated
+     */
+    public void setOrderByClause(String orderByClause) {
+        this.orderByClause = orderByClause;
+    }
+
+    /**
+     *
+     * @mbggenerated
+     */
+    public String getOrderByClause() {
+        return orderByClause;
+    }
+
+    /**
+     *
+     * @mbggenerated
+     */
+    public void setDistinct(boolean distinct) {
+        this.distinct = distinct;
+    }
+
+    /**
+     *
+     * @mbggenerated
+     */
+    public boolean isDistinct() {
+        return distinct;
+    }
+
+    /**
+     *
+     * @mbggenerated
+     */
+    public List<Criteria> getOredCriteria() {
+        return oredCriteria;
+    }
+
+    /**
+     *
+     * @mbggenerated
+     */
+    public void or(Criteria criteria) {
+        oredCriteria.add(criteria);
+    }
+
+    /**
+     *
+     * @mbggenerated
+     */
+    public Criteria or() {
+        Criteria criteria = createCriteriaInternal();
+        oredCriteria.add(criteria);
+        return criteria;
+    }
+
+    /**
+     *
+     * @mbggenerated
+     */
+    public Criteria createCriteria() {
+        Criteria criteria = createCriteriaInternal();
+        if (oredCriteria.size() == 0) {
+            oredCriteria.add(criteria);
+        }
+        return criteria;
+    }
+
+    /**
+     *
+     * @mbggenerated
+     */
+    protected Criteria createCriteriaInternal() {
+        Criteria criteria = new Criteria();
+        return criteria;
+    }
+
+    /**
+     *
+     * @mbggenerated
+     */
+    public void clear() {
+        oredCriteria.clear();
+        orderByClause = null;
+        distinct = false;
+    }
+
+    /**
+     * alarm_policy_object null
+     */
+    protected abstract static class GeneratedCriteria {
+        protected List<Criterion> criteria;
+
+        protected GeneratedCriteria() {
+            super();
+            criteria = new ArrayList<Criterion>();
+        }
+
+        public boolean isValid() {
+            return criteria.size() > 0;
+        }
+
+        public List<Criterion> getAllCriteria() {
+            return criteria;
+        }
+
+        public List<Criterion> getCriteria() {
+            return criteria;
+        }
+
+        protected void addCriterion(String condition) {
+            if (condition == null) {
+                throw new RuntimeException("Value for condition cannot be null");
+            }
+            criteria.add(new Criterion(condition));
+        }
+
+        protected void addCriterion(String condition, Object value, String property) {
+            if (value == null) {
+                throw new RuntimeException("Value for " + property + " cannot be null");
+            }
+            criteria.add(new Criterion(condition, value));
+        }
+
+        protected void addCriterion(String condition, Object value1, Object value2, String property) {
+            if (value1 == null || value2 == null) {
+                throw new RuntimeException("Between values for " + property + " cannot be null");
+            }
+            criteria.add(new Criterion(condition, value1, value2));
+        }
+
+        public Criteria andPolicyIdIsNull() {
+            addCriterion("policy_id is null");
+            return (Criteria) this;
+        }
+
+        public Criteria andPolicyIdIsNotNull() {
+            addCriterion("policy_id is not null");
+            return (Criteria) this;
+        }
+
+        public Criteria andPolicyIdEqualTo(String value) {
+            addCriterion("policy_id =", value, "policyId");
+            return (Criteria) this;
+        }
+
+        public Criteria andPolicyIdNotEqualTo(String value) {
+            addCriterion("policy_id <>", value, "policyId");
+            return (Criteria) this;
+        }
+
+        public Criteria andPolicyIdGreaterThan(String value) {
+            addCriterion("policy_id >", value, "policyId");
+            return (Criteria) this;
+        }
+
+        public Criteria andPolicyIdGreaterThanOrEqualTo(String value) {
+            addCriterion("policy_id >=", value, "policyId");
+            return (Criteria) this;
+        }
+
+        public Criteria andPolicyIdLessThan(String value) {
+            addCriterion("policy_id <", value, "policyId");
+            return (Criteria) this;
+        }
+
+        public Criteria andPolicyIdLessThanOrEqualTo(String value) {
+            addCriterion("policy_id <=", value, "policyId");
+            return (Criteria) this;
+        }
+
+        public Criteria andPolicyIdLike(String value) {
+            addCriterion("policy_id like", value, "policyId");
+            return (Criteria) this;
+        }
+
+        public Criteria andPolicyIdNotLike(String value) {
+            addCriterion("policy_id not like", value, "policyId");
+            return (Criteria) this;
+        }
+
+        public Criteria andPolicyIdIn(List<String> values) {
+            addCriterion("policy_id in", values, "policyId");
+            return (Criteria) this;
+        }
+
+        public Criteria andPolicyIdNotIn(List<String> values) {
+            addCriterion("policy_id not in", values, "policyId");
+            return (Criteria) this;
+        }
+
+        public Criteria andPolicyIdBetween(String value1, String value2) {
+            addCriterion("policy_id between", value1, value2, "policyId");
+            return (Criteria) this;
+        }
+
+        public Criteria andPolicyIdNotBetween(String value1, String value2) {
+            addCriterion("policy_id not between", value1, value2, "policyId");
+            return (Criteria) this;
+        }
+
+        public Criteria andObjectIdIsNull() {
+            addCriterion("object_id is null");
+            return (Criteria) this;
+        }
+
+        public Criteria andObjectIdIsNotNull() {
+            addCriterion("object_id is not null");
+            return (Criteria) this;
+        }
+
+        public Criteria andObjectIdEqualTo(String value) {
+            addCriterion("object_id =", value, "objectId");
+            return (Criteria) this;
+        }
+
+        public Criteria andObjectIdNotEqualTo(String value) {
+            addCriterion("object_id <>", value, "objectId");
+            return (Criteria) this;
+        }
+
+        public Criteria andObjectIdGreaterThan(String value) {
+            addCriterion("object_id >", value, "objectId");
+            return (Criteria) this;
+        }
+
+        public Criteria andObjectIdGreaterThanOrEqualTo(String value) {
+            addCriterion("object_id >=", value, "objectId");
+            return (Criteria) this;
+        }
+
+        public Criteria andObjectIdLessThan(String value) {
+            addCriterion("object_id <", value, "objectId");
+            return (Criteria) this;
+        }
+
+        public Criteria andObjectIdLessThanOrEqualTo(String value) {
+            addCriterion("object_id <=", value, "objectId");
+            return (Criteria) this;
+        }
+
+        public Criteria andObjectIdLike(String value) {
+            addCriterion("object_id like", value, "objectId");
+            return (Criteria) this;
+        }
+
+        public Criteria andObjectIdNotLike(String value) {
+            addCriterion("object_id not like", value, "objectId");
+            return (Criteria) this;
+        }
+
+        public Criteria andObjectIdIn(List<String> values) {
+            addCriterion("object_id in", values, "objectId");
+            return (Criteria) this;
+        }
+
+        public Criteria andObjectIdNotIn(List<String> values) {
+            addCriterion("object_id not in", values, "objectId");
+            return (Criteria) this;
+        }
+
+        public Criteria andObjectIdBetween(String value1, String value2) {
+            addCriterion("object_id between", value1, value2, "objectId");
+            return (Criteria) this;
+        }
+
+        public Criteria andObjectIdNotBetween(String value1, String value2) {
+            addCriterion("object_id not between", value1, value2, "objectId");
+            return (Criteria) this;
+        }
+    }
+
+    /**
+     * alarm_policy_object
+     */
+    public static class Criteria extends GeneratedCriteria {
+
+        protected Criteria() {
+            super();
+        }
+    }
+
+    /**
+     * alarm_policy_object null
+     */
+    public static class Criterion {
+        private String condition;
+
+        private Object value;
+
+        private Object secondValue;
+
+        private boolean noValue;
+
+        private boolean singleValue;
+
+        private boolean betweenValue;
+
+        private boolean listValue;
+
+        private String typeHandler;
+
+        public String getCondition() {
+            return condition;
+        }
+
+        public Object getValue() {
+            return value;
+        }
+
+        public Object getSecondValue() {
+            return secondValue;
+        }
+
+        public boolean isNoValue() {
+            return noValue;
+        }
+
+        public boolean isSingleValue() {
+            return singleValue;
+        }
+
+        public boolean isBetweenValue() {
+            return betweenValue;
+        }
+
+        public boolean isListValue() {
+            return listValue;
+        }
+
+        public String getTypeHandler() {
+            return typeHandler;
+        }
+
+        protected Criterion(String condition) {
+            super();
+            this.condition = condition;
+            this.typeHandler = null;
+            this.noValue = true;
+        }
+
+        protected Criterion(String condition, Object value, String typeHandler) {
+            super();
+            this.condition = condition;
+            this.value = value;
+            this.typeHandler = typeHandler;
+            if (value instanceof List<?>) {
+                this.listValue = true;
+            } else {
+                this.singleValue = true;
+            }
+        }
+
+        protected Criterion(String condition, Object value) {
+            this(condition, value, null);
+        }
+
+        protected Criterion(String condition, Object value, Object secondValue, String typeHandler) {
+            super();
+            this.condition = condition;
+            this.value = value;
+            this.secondValue = secondValue;
+            this.typeHandler = typeHandler;
+            this.betweenValue = true;
+        }
+
+        protected Criterion(String condition, Object value, Object secondValue) {
+            this(condition, value, secondValue, null);
+        }
+    }
+}
